83df0719b384b99743ef24882613e254ba1c1cfe
Iván Todorovich <ivan.todorovich@gmail.com>
2019-08-15 12:19:17 +0000

    [MIG] Migration to 12.0: airline

---
+++ airline/models/res_partner.py
@@ -0,0 +1,11 @@
+# Copyright 2013 Savoir-faire Linux (<http://www.savoirfairelinux.com>)
+# Copyright 2019 Druidoo
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import models, fields
+
+
+class ResPartner(models.Model):
+    _inherit = 'res.partner'
+
+    airline = fields.Boolean()
