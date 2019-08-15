b74f11fd8cab77eecd482d64dfb274163ce7daf8
Iván Todorovich <ivan.todorovich@gmail.com>
2019-08-15 14:41:04 +0000

    [MIG] Migration to 12.0 : airline_data

--- airline_data/__manifest__.py
+++ airline_data/__manifest__.py
@@ -1,51 +1,20 @@
-# -*- encoding: utf-8 -*-
-##############################################################################
-#
-#    OpenERP, Open Source Management Solution
-#    This module copyright (C) 2013 Savoir-faire Linux
-#    (<http://www.savoirfairelinux.com>).
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-##############################################################################
+# Copyright 2013 Savoir-faire Linux (<http://www.savoirfairelinux.com>)
+# Copyright 2019 Druidoo
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
-    'name': 'Partner Airline: Module Data',
-    'version': '0.1',
+    'name': 'Partner Airline: Data Module',
+    'version': '12.0.1.0.0',
+    'summary': 'Contains airline data',
     'category': 'Customer Relationship Management',
-    'description': """
-Partner Airline: Module Data
-============================
-
-Core Data for Partner Airline, is kept as a separate module to overcome the
-need to localize core data as well as to avoid long load times.
-
-Contributors
-------------
-* Sandy Carter (sandy.carter@savoirfairelinux.com)
-* El Hadji Dem (elhadji.dem@savoirfairelinux.com)
-    """,
-    'author': "Savoir-faire Linux,Odoo Community Association (OCA)",
-    'website': 'http://www.savoirfairelinux.com',
+    'author': 'Savoir-faire Linux, '
+              'Odoo Community Association (OCA)',
+    'website': 'https://github.com/OCA/vertical-travel',
     'license': 'AGPL-3',
-    'depends': ['airline', ],
-    'external_dependencies': {},
+    'depends': [
+        'airline',
+    ],
     'data': [
         'data/airline_data.xml',
     ],
-    'demo': [],
-    'test': [],
-    'installable': False,
-    'auto_install': False,
-    'images': [],
 }
